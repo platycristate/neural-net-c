98ec9248ad039728cee1592e4cd97d996e11f425
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-14 19:43:45 +0300

    working Matrix class

--- Test.h
+++ Test.h
@@ -19,29 +19,30 @@ bool compareMatrices(const Matrix &a, const Matrix &b) {
 }
 
 void readData(){
-    std::ifstream file;
-    file.open("../tests.txt");
+    std::ifstream file("../tests.txt");
     std::string line;
     // Read a file
     if (file.is_open()) {
-        while (file.good()){
+        std::vector<std::vector<float>> array;
+        while (file.good()) {
             std::getline(file, line);
             std::istringstream iss(line);
-            std::vector<float> array;
             float element;
-            while (iss.good()){
+            std::vector<float> row;
+            while (iss.good()) {
                 iss >> element;
-                array.push_back(element);
+                row.push_back(element);
             }
-            std::cout << array.size() << std::endl;
-
+            array.push_back(row);
         }
+        Matrix mat(array);
+        mat.printArray();
+        file.close();
     }
-    file.close();
 }
 
 void Test(){
-//    std::cout << "Testing addition" << std::endl;
+    std::cout << "Testing addition" << std::endl;
     std::vector<std::vector<float>> data = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     std::vector<std::vector<float>> data2 = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
     std::vector<std::vector<float>> data_cor = {{2, 3, 4}, {5, 6, 7}, {8, 9, 10}};
@@ -63,9 +64,9 @@ void Test(){
     Matrix mat4(data4), mat5(data5), res_cor2(data_cor2);
     Matrix res2 = mat4 + mat5;
     assert(compareMatrices(res2, res_cor2) == true);
-//    std::cout << "Finished addition" << std::endl;
+    std::cout << "Finished addition" << std::endl;
 
-//    std::cout << "Testing transpositions" << std::endl;
+    std::cout << "Testing transpositions" << std::endl;
     std::vector<std::vector<float>> data6 = {{1}};
     Matrix mat6(data6);
     Matrix mat7 = mat6.transpose();
@@ -76,8 +77,19 @@ void Test(){
     Matrix mat8(data8), res_cor8(data_cor8);
     Matrix res8 = mat8.transpose();
     assert(compareMatrices(res8, res_cor8) == true);
-//    std::cout << "Finished Testing" << std::endl;
 
-    // Tests from file
-    readData();
+    // Multiplication tests
+    std::vector<std::vector<float>> data9 = {{1, -3}};
+    std::vector<std::vector<float>> data10 = {{4}, {2}};
+    std::vector<std::vector<float>> data_cor9 = {{-2}};
+    std::vector<std::vector<float>> data_cor10 = {{4, -12}, {2, -6}};
+    Matrix mat9(data9), mat10(data10),  res_cor9(data_cor9), res_cor10(data_cor10);
+    Matrix res9 = mat9 * mat10;
+    Matrix res10 = mat10 * mat9;
+    res9.printArray();
+    res10.printArray();
+    assert(compareMatrices(res9, res_cor9) == true);
+    assert(compareMatrices(res10, res_cor10) == true);
+
+    std::cout << "Finished Testing" << std::endl;
 }
