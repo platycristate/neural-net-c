1fdff6c289d98570eb991bce1964e3a06d5e0c1c
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-14 21:17:05 +0300

    Follow up commit

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-#include "Test.h"
+#include <Test_matrix.h>
+#include <Net.h>
 #include <vector>
 
 int main() {
     std::vector<std::vector<float>> data = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
     std::vector<std::vector<float>> data2 = {{1, 1, 1}, {1, 1, 1}, {1, 1, 1}};
     Test();
+    Net net1(2, 3);
     return 0;
 }
