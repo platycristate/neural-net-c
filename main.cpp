1570e655b01838e597b1a770a3c5bc131bbb19f1
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-16 22:33:31 +0300

    example

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
 #include <Matrix.h>
-#include <Modules.h>
 #include <vector>
-#include <tuple>
 #include <Network.h>
+#include <Loss_functions.h>
 
 int main() {
     std::vector<std::vector<double>> input_data = {{4}, {-2}};
+    std::vector<std::vector<double>> target_data = {{0, 1, 1, 1, 0, 0, 1, 0}};
+    std::vector<std::vector<int>> dims = {{8, 2}, {8, 8}};
+
     Matrix input(input_data);
-    std::vector<std::vector<int>> dims = {{8, 2}, {1, 8}};
+    Matrix target(target_data);
+    target = target.transpose();
     Network net(dims);
-    Matrix res = net.forward(input);
-    Matrix grad_output(1, 1, 1);
+
+    // Forward part
+    Matrix pred = net.forward(input);
+    pred = ReLU::forward(pred);
+
+    // Backward part
+    Matrix loss = CrossEntropyLoss::forward(pred, target);
+    Matrix grad_output = CrossEntropyLoss::backward(pred, target, loss);
+    grad_output.printArray();
+    grad_output.shape();
+    grad_output = ReLU::backward(grad_output, pred);
     net.backward(grad_output);
-    res.printArray();
-    net.layers[0].grad_weight.printArray();
+    net.layers[1].weight.printArray();
+    net.layers[1].weight.shape();
     return 0;
 }
