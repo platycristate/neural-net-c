90bf9ce6b4d31a32be8eb311118b8bac1a4f7f93
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-16 22:05:09 +0300

    add cross entropy loss function

---
+++ neural_net/Loss_functions.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <cassert>
+#include <cmath>
+#include <Matrix.h>
+
+struct CrossEntropyLoss {
+    static Matrix forward(Matrix &pred, Matrix &target ) {
+        assert(target.n_rows == pred.n_rows);
+        double result = 0;
+        for (int i=0; i < pred.n_rows; i++) {
+            result -= target.data[i][0] * std::log1p(pred.data[i][0]);
+        }
+        return Matrix{1, 1, result};
+    }
+    static Matrix backward(
+            Matrix &pred,
+            Matrix &target,
+            Matrix &output) {
+        Matrix grad_output_input(pred.n_rows, pred.n_cols);
+        double grad_w;
+        for (int row=0; row < pred.n_rows; row++) {
+            grad_w = target.data[row][0] * 1 / (pred.data[row][0] + 1e-8);
+            grad_output_input.data[row][0] = grad_w;
+        }
+        return grad_output_input.transpose();
+    }
+};
