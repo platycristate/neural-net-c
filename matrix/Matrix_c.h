870712cf88a91a284e8d2d45f777d208ebd1b9f2
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-19 11:59:56 +0300

    new matrix class works fine

--- matrix/Matrix_c.h
+++ matrix/Matrix_c.h
@@ -25,15 +25,60 @@ struct Matrix {
                 *(data + row*n_cols + col) = value;
         }
     }
+    Matrix operator * (Matrix const &mat) {
+        assert(n_cols == mat.n_rows);
+        double * out = (double *) malloc(sizeof(double) * n_rows * mat.n_cols);
+        for (int row=0; row < n_rows; row++) {
+            for (int col=0; col < mat.n_cols; col++) {
+                double value = 0;
+                for (int i=0; i < n_cols; i++) {
+                     value += *(data + row*n_cols + i) * *(mat.data + i*mat.n_cols + col);
+                }
+                *(out + row*mat.n_cols + col) = value;
+            }
+        }
+        Matrix out_mat(out, n_rows, mat.n_cols);
+        return out_mat;
+    }
+    Matrix scalarMul(double const f) const {
+        double * out = (double *) malloc(sizeof(double) * n_rows * n_cols);
+        for (int row=0; row < n_rows; row++) {
+            for (int col=0; col < n_cols; col++)
+                *(out + row*n_cols + col) = f * *(data + row*n_cols + col);
+        }
+        Matrix out_mat(out, n_rows, n_cols);
+        return out_mat;
+    }
+    Matrix operator ^ (Matrix const &mat) {
+        assert(n_rows == mat.n_rows && n_cols == mat.n_cols);
+        double * out = (double *) malloc(sizeof(double) * n_rows * n_cols);
+        for (int row=0; row < n_rows; row++) {
+            for (int col=0; col < n_cols; col++)
+                *(out + row*n_cols + col) = *(data + row*n_cols + col) * *(mat.data + row*n_cols + col);
+        }
+        Matrix out_mat(out, n_rows, n_cols);
+        return out_mat;
+    }
     Matrix operator + (Matrix const &mat) {
         assert(mat.n_cols == n_cols && mat.n_rows == n_rows);
-        double * out = (double*) malloc(sizeof(double) * n_rows * n_cols);
+        double * out = (double *) malloc(sizeof(double) * n_rows * n_cols);
         for (int row=0; row < n_rows; row++) {
             for (int col=0; col < n_cols; col++)
                 *(out + row*n_cols + col) =
                 *(data + row*n_cols + col) + *(mat.data + row*n_cols + col);
         }
         static Matrix out_mat(out, n_rows, n_cols);
+        return out_mat;
+    }
+    Matrix transpose() {
+        double * out = (double *) malloc(sizeof(double) * n_rows * n_cols);
+        for (int row=0; row < n_rows; row++) {
+            for (int col=0; col < n_cols; col++) {
+                *(out + row*n_rows + col) = *(data + col*n_cols + row);
+                *(out + col*n_rows + row) = *(data + row*n_cols + col);
+            }
+        }
+        Matrix out_mat(out, n_cols, n_rows);
         return out_mat;
     }
 
--- matrix/main.cpp
+++ matrix/main.cpp
@@ -3,13 +3,11 @@
 
 
 int main() {
-    double data[6] = {1.0, 2.0, 3, 4, 5, 6};
-    double data2[6] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0};
-    unsigned int n_rows = 2;
-    unsigned int n_cols = 3;
-    Matrix mat1(data, n_rows, n_cols);
-    Matrix mat2(data2, n_rows, n_cols);
-    Matrix mat3 = mat1 + mat2;
-    mat3.print();
+    double data1[3] = {-1, 1, 3};
+    Matrix mat1(data1, 3, 1);
+    mat1.print();
+    std::cout << '\n';
+    Matrix mat2 = mat1.transpose();
+    mat2.print();
     return 0;
 }
