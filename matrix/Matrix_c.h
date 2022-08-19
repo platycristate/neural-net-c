f1613d80ff42caeaf3a41934c5c2af5a0eed3548
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-19 14:50:29 +0300

    add operator -

--- matrix/Matrix_c.h
+++ matrix/Matrix_c.h
@@ -75,6 +75,11 @@ struct matrix {
         static matrix out_mat(out, n_rows, n_cols);
         return out_mat;
     }
+    matrix operator - (matrix const &mat) const {
+        assert(mat.n_cols == n_cols && mat.n_rows == n_rows);
+        matrix out_mat = *(this) + mat.scalarMul(-1);
+        return out_mat;
+    }
     double get(int const i, int const j) const {
         assert(i < n_rows && j < n_cols);
         return *(data + n_cols*i + j);
@@ -99,7 +104,9 @@ struct matrix {
         n_rows = n_rows_;
         n_cols = n_cols_;
     }
-
+    void shape() const {
+        std::cout << "(" << n_rows << ", " << n_cols << ")\n";
+    }
     void print() const {
         for (int row=0; row < n_rows; row++) {
             for (int col=0; col < n_cols; col++)
