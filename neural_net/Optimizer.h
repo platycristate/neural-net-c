f620de227d961a89733cb12ddd07efd6635503af
rcentea <arsentii.ivasiuk@gmail.com>
2022-08-19 14:50:47 +0300

    refactored Optimizer.h

--- neural_net/Optimizer.h
+++ neural_net/Optimizer.h
@@ -1,25 +1,30 @@
 #pragma once
 #include <iostream>
 #include <vector>
-#include <Matrix.h>
+//#include <Matrix.h>
+#include <Matrix_c.h>
 #include <Network.h>
 
 struct Optimizer {
     double learning_rate;
-    std::vector<LinearLayer *> layer_ptrs;
+    int n_ptrs;
+    LinearLayer ** layer_ptrs;
 
     explicit Optimizer( Network &net, double lr) {
         learning_rate = lr;
         LinearLayer * layer_ptr;
-        unsigned int n_layers = net.dims.size();
-        for (int i=0; i < n_layers; i++) {
+        n_ptrs = net.dims.size();
+        layer_ptrs = (LinearLayer **) malloc(sizeof(LinearLayer*) * n_ptrs);
+        for (int i=0; i < n_ptrs; i++) {
             layer_ptr = &(net.layers[i]);
-            layer_ptrs.push_back(layer_ptr);
+            *(layer_ptrs + i) = layer_ptr;
         }
     }
     void gradient_step() {
-        Matrix change{1, 1};
-        for (auto & layer_ptr : layer_ptrs) {
+        matrix change{1, 1};
+        LinearLayer * layer_ptr;
+        for (int i=0; i < n_ptrs; i++) {
+            layer_ptr = layer_ptrs[i];
             change = layer_ptr->grad_weight.scalarMul(learning_rate);
             layer_ptr->weight = layer_ptr->weight - change;
             change = layer_ptr->grad_bias.scalarMul(learning_rate);
